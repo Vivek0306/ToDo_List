48589665b7154f0c215e6df3309ed6549524487e
Vivek Nair <vivekmanju53@gmail.com>
2022-10-28 01:13:49 +0530

    Added flask layouts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, request, render_template, request, redirect, url_for
+from flask import Flask, request, render_template, request, redirect, url_for, session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import false
 
@@ -9,6 +10,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
@@ -17,7 +24,14 @@ class ToDo(db.Model):
 @app.route("/")
 def index():
     todo_list = ToDo.query.all()
-    return render_template('base.html', todo_list=todo_list)
+    return render_template('main.html', todo_list=todo_list)
+
+@app.route("/register")
+def register():
+    # email = request.form.get("email")
+    # uname = request.form.get("uname")
+    # password = request.form.get("password")
+    return render_template('register.html')
 
 @app.route("/add", methods=["POST"])
 def add():
